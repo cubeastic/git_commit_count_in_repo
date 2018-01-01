bf1617f1d821e823d604ccb39b95d554af24ac98
Dennis Zheleznyak <>
2018-01-01 18:05:05 +0200

    fixed commit counter and added branch support
    Branch is not working yet

--- git_commit_count.py
+++ git_commit_count.py
@@ -1,10 +1,10 @@
-from requests import get, post
-from time import ctime, strftime, localtime, time
+from requests import get
+from time import strftime, localtime, mktime, strptime
+from datetime import datetime
 from os import path
 from sys import exit
 import xml.etree.ElementTree as Et  # Import the function for xml handling
 from beautifultable import BeautifulTable
-from random import randint
 
 
 class GitCounter:
@@ -14,9 +14,12 @@ class GitCounter:
         if path.exists(self.config_file):
             self.token = self.get_config("git_token")
             self.repos_file = self.get_config("repos_file")
-            self.time_frame = self.get_config("last_X_months")
+            self.from_date = int(mktime(strptime(self.get_config("from_date"), "%d/%m/%Y")))
+            self.to_date = int(mktime(strptime(self.get_config("to_date"), "%d/%m/%Y")))
             self.repositories = dict()
-            self.api_addr = "https://api.github.com/repos/{0}/{1}/stats/commit_activity"
+            self.commit_url = "https://api.github.com/repos/{0}/{1}/stats/commit_activity"
+            self.branches_url = "https://api.github.com/repos/{0}/{1}/branches"
+            self.branch_url = "https://api.github.com/repos/{0}/{1}/branches/{2}"
             self.headers = {'Authorization': 'token {}'.format(self.token)}
             self.errors = (401, 400, 403, 404)
 
@@ -35,40 +38,51 @@ class GitCounter:
         if path.exists(self.repos_file):
             with open(self.repos_file) as f:
                 for i in f:
-                    self.repositories[i.split("/")[3]] = i.split("/")[4].replace("\n", "")
+                    self.repositories[i.split("/")[3]] = [i.split("/")[4].replace("\n", ""), 0]
+            return True
         else:
             print("ERROR: {0} was not found".format(self.repos_file))
             exit()
-
-    def unix_to_time(self, t):
-        return strftime("%Y-%m-%d", localtime(int(t)))
 
     def count_commits(self):
         if get("https://api.github.com", headers=self.headers).status_code not in self.errors:
             if self.repositories:
                 for owner, repo in self.repositories.items():
-                    json_obj = get(self.api_addr.format(owner, repo), headers=self.headers).json()
-                    for elem in json_obj:
-                        try:
-                            if self.last_X_months(elem["week"]):
-                                print self.unix_to_time(elem["week"])
-                        except:
-                            print json_obj["message"]
+                    json_obj = get(self.commit_url.format(owner, repo[0]), headers=self.headers).json()
+                    for o in json_obj:
+                        if self.from_date < o["week"] < self.to_date:
+                            repo[1] += o["total"]
+                return True
         else:
             print("ERROR: please enter a valid token key")
 
-    def last_X_months(self, t):
-        return abs(t - time()) < (2592000 * self.time_frame)
+    def branch_check(self):
+        if get("https://api.github.com", headers=self.headers).status_code not in self.errors:
+            if self.repositories:
+                for owner, repo in self.repositories.items():
+                    json_obj = get(self.branches_url.format(owner, repo[0]), headers=self.headers).json()
+                    if len(json_obj) >= 1:
+                        for o in json_obj:
+                            sjson_obj = get(self.branch_url.format(owner, repo[0], o["name"]), headers=self.headers).json()
+                            for so in sjson_obj:
+                                print(so)
+
+                return True
 
     def build_table(self):
-        table = BeautifulTable()
-        table.column_headers = ["Owner", "Name", "Commits"]
-        for owner, repo in self.repositories.items():
-            table.append_row([owner, repo, randint(1, 99)])
-        table.sort("Commits")
-        return table
+        if self.count_commits() and self.branch_check():
+            table = BeautifulTable()
+            table.column_headers = ["Name", "Commits", "New Branches"]
+            for owner, repo in self.repositories.items():
+                table.append_row([repo[0], repo[1], "test"])
+            return table
 
 
-a = GitCounter()
-a.get_list()
-a.count_commits()
+if __name__ == "__main__":
+    a = GitCounter()
+    if a.get_list():
+        print(a.build_table())
+
+        # Convert from timestamp
+        # value = datetime.fromtimestamp(timestamp)
+        # print(value.strftime('%d-%m-%Y'))
